7c77ccdeb379c371c566a6da02e0495b051b3efd
starlight36 <starlight36@gmail.com>
2026-09-04 08:36:47 +0800

    signal-light 0.1.3

--- Formula/signal-light.rb
+++ Formula/signal-light.rb
@@ -1,26 +1,26 @@
 class SignalLight < Formula
   desc "Local signal-light runtime for AI agent status display"
   homepage "https://github.com/starlight36/vibecoding-signal-light"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.2/signal-light-v0.1.2-macos-aarch64.tar.gz"
-      sha256 "e56489e76bdc1e1704fe27ed8d501cfbf8d1caa40ed17b84e24e4e3adb3a57e2"
+      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.3/signal-light-v0.1.3-macos-aarch64.tar.gz"
+      sha256 "8641482a966d50b13ea56b52d1ed64d23ca286802b551436776006d5e911edc5"
     else
-      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.2/signal-light-v0.1.2-macos-x86_64.tar.gz"
-      sha256 "aba0496a5ac67e88fba3f5f2053bbc012aeaa907ed326db483a8928150dae6ed"
+      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.3/signal-light-v0.1.3-macos-x86_64.tar.gz"
+      sha256 "87b15a1bbd507f369a9c35c56a8a221255272e5c28a6dfda740ed9415bdd95d1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.2/signal-light-v0.1.2-linux-arm64.tar.gz"
-      sha256 "9472cc56de3fc8d1121eaeeea8ee7b4afca8d98bf1eae7e010597134d31e2722"
+      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.3/signal-light-v0.1.3-linux-arm64.tar.gz"
+      sha256 "e09c37b6310fa171093c274bbee848a394faf67b830f72b376ac0e74e1627daa"
     else
-      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.2/signal-light-v0.1.2-linux-amd64.tar.gz"
-      sha256 "55b08ba1eada8c7b51f96d5cab6c9e7682dfa5d78e7dbcdfb997b8833d5e6795"
+      url "https://github.com/starlight36/vibecoding-signal-light/releases/download/v0.1.3/signal-light-v0.1.3-linux-amd64.tar.gz"
+      sha256 "f952ec66eda367cc3e4ddec254161baf33ad561009cbc28bced686b887b20680"
     end
   end
 
